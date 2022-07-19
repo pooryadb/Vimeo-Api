a051af513a2786aa091f0042df15b62026d2b6d0
pooryadb <dev.poorya.db@gmail.com>
2022-07-19 12:48:25 +0430

    fix fullscreen

--- app/src/main/java/db/poorya/namavatest/ext/ActivityExt.kt
+++ app/src/main/java/db/poorya/namavatest/ext/ActivityExt.kt
@@ -33,8 +33,6 @@ fun Activity.disableFullScreen() {
         window.insetsController?.show(WindowInsets.Type.systemBars())
     } else {
         @Suppress("DEPRECATION")
-        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                /*or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN*/)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE)
     }
 }
